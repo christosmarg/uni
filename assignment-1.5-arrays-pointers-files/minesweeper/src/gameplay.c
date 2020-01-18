91f6f902152890d796bb393982482b21f26e42b9
Christos Margiolis <christos@margiolis.net>
2020-01-18 14:56:35 +0200

    pending gameWin dimension fix

--- assignment-1.5-arrays-pointers-files/minesweeper/src/gameplay.c
+++ assignment-1.5-arrays-pointers-files/minesweeper/src/gameplay.c
@@ -4,7 +4,7 @@
 
 void selection(WINDOW *gameWin, char **dispboard, char **mineboard, int WIDTH, int HEIGHT)
 {
-    int chrow, chcol;
+    int chRow, chCol;
     bool gameOver;
     int yMax, xMax, yMiddle, xMiddle;
     getmaxyx(stdscr, yMax, xMax);
@@ -14,44 +14,44 @@ void selection(WINDOW *gameWin, char **dispboard, char **mineboard, int WIDTH, i
     do
     {
         mvprintw(1, 1, "Choice (row, col): ");
-        scanw("%d %d", &chrow, &chcol);
-        chrow++;
-        chcol++;
-        mvprintw(1, strlen("Choice (row, col): ") + 1, "(%d,%d)", chrow-1, chcol-1);
+        scanw("%d %d", &chRow, &chCol);
+        chRow++;
+        chCol++;
+        mvprintw(1, strlen("Choice (row, col): ") + 1, "(%d,%d)", chRow-1, chCol-1);
         refresh();
         mvprintw(1, 1, CLEAR);
-        gameOver = transfer(dispboard, mineboard, chrow, chcol);
-        reveal(gameWin, dispboard, chrow, chcol);
+        gameOver = transfer(dispboard, mineboard, chRow, chCol);
+        reveal(gameWin, dispboard, chRow, chCol);
         getchar();
-    } while (chrow < 0 || chrow > WIDTH-1 || chcol < 0 || chcol > HEIGHT-1 || !gameOver);
+    } while (chRow < 0 || chRow > WIDTH-1 || chCol < 0 || chCol > HEIGHT-1 || !gameOver);
     
     if (gameOver == true)
     {
         game_over(gameWin, mineboard, yMiddle, xMiddle);
         getchar();
         print_board(gameWin, mineboard, WIDTH, HEIGHT);
-        filewrite(mineboard, WIDTH, HEIGHT, chrow, chcol);
+        filewrite(mineboard, WIDTH, HEIGHT, chRow, chCol);
     }
 }
 
 
-bool transfer(char **dispboard, char **mineboard, int chrow, int chcol)
+bool transfer(char **dispboard, char **mineboard, int chRow, int chCol)
 {
-    dispboard[chrow][chcol] = mineboard[chrow][chcol];
-    return (dispboard[chrow][chcol] == MINE) ? true : false;
+    dispboard[chRow][chCol] = mineboard[chRow][chCol];
+    return (dispboard[chRow][chCol] == MINE) ? true : false;
 }
 
 
-void reveal(WINDOW *gameWin, char **dispboard, int chrow, int chcol)
+void reveal(WINDOW *gameWin, char **dispboard, int chRow, int chCol)
 {
-    mvwaddch(gameWin, chrow, chcol, dispboard[chrow][chcol]);
+    mvwaddch(gameWin, chRow, chCol, dispboard[chRow][chCol]);
     wrefresh(gameWin);
 }
 
 void game_over(WINDOW *gameWin, char **mineboard, int yMiddle, int xMiddle)
 {
     wclear(gameWin);
-    mvwprintw(gameWin, yMiddle-11, xMiddle-12, "You hit a mine!");
+    mvwprintw(gameWin, yMiddle-11, xMiddle-11, "You hit a mine!");
     mvwprintw(gameWin, yMiddle-10, xMiddle-10, "Game over :(");
     wrefresh(gameWin);
     wclear(gameWin);
