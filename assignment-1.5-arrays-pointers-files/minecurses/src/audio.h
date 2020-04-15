dc22358231944047f9d0f0237b62196c4d2c1f1f
Christos Margiolis <christos@margiolis.net>
2020-04-15 05:07:33 +0300

    updated submodule

--- assignment-1.5-arrays-pointers-files/minecurses/src/audio.h
+++ assignment-1.5-arrays-pointers-files/minecurses/src/audio.h
@@ -5,7 +5,7 @@
 #include <SDL2/SDL_mixer.h>
 #include "minesweeper.h"
 
-#define AUDIO_PATH "audio/detective-8bit.wav"
+#define AUDIO_PATH "res/detective-8bit.wav"
 
 void *play_audio(void *threadid);
 void volume		(char);
