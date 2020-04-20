30dec0f2579f320bc1ade02a230d1498e3de511a
Christos Margiolis <christos@margiolis.net>
2020-04-20 16:34:49 +0300

    removed inlines

--- assignment-2.3-operoverloading/src/subject.h
+++ assignment-2.3-operoverloading/src/subject.h
@@ -16,13 +16,13 @@ class Subject
 		Subject(const std::string& code, const std::string& sname, unsigned int subjsemester);
 		Subject(const Subject& s);
 
-		inline const std::string& get_code(void) const {return this->code;}
-		inline const std::string& get_sname(void) const {return this->sname;}
-		inline unsigned int get_subjsemester(void) const {return this->subjsemester;}
+		const std::string& get_code() const;
+		const std::string& get_sname() const;
+		unsigned int get_subjsemester() const;
 
-		inline void set_code(const std::string& code) {this->code = code;}
-		inline void set_sname(const std::string& sname) {this->sname = sname;}
-		inline void set_subjsemester(unsigned int subjsemester) {this->subjsemester = subjsemester;}
+		void set_code(const std::string& code);
+		void set_sname(const std::string& sname);
+		void set_subjsemester(unsigned int subjsemester);
 };
 
 #endif /* SUBJECT_H */
