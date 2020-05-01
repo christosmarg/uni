5b729bc0bfee1ea955b77d341b26a55b57517c4a
Christos Margiolis <christos@margiolis.net>
2020-05-01 03:52:25 +0300

    improved xstring

--- assignment-2.5-spreadsheets/src/main.cpp
+++ assignment-2.5-spreadsheets/src/main.cpp
@@ -1,8 +1,7 @@
-#include <vector>
 #include "xstring.h"
 
 int main(int argc, char **argv)
 {
-		
+
 	return 0;
 }
--- assignment-2.5-spreadsheets/src/xstring.h
+++ assignment-2.5-spreadsheets/src/xstring.h
@@ -2,6 +2,7 @@
 #define XSTRING_H
 
 #include <algorithm>
+#include <cstdlib>
 #include <iostream>
 #include <string.h>
 
@@ -11,20 +12,58 @@ class xstring
 		char *str;
 		int len;
 
-		char *transfer(const char *str);
+		char *conv(const char *s);
+		void resize(const char *s);
+		bool strempty(const char *s) const;
 
 	public:
 		xstring();
-		xstring(const char *str);
+		xstring(const char *s);
 		xstring(const xstring& s);
 		~xstring();
 		
-		void operator+=	(const char *ns);
-		xstring operator= (const xstring& s);
+		xstring	 operator=	(const xstring& s);
+		xstring	 operator=	(const char *s);
+		xstring  operator+	(const xstring& s);
+		xstring  operator+	(const char *s);
+		xstring& operator+=	(const xstring& s);
+		xstring& operator+=	(const char *s);
+		bool operator==	(const xstring& s)	const;
+		bool operator== (const char *s)		const;
+		bool operator!=	(const xstring& s)	const;
+		bool operator!= (const char *s)		const;
+		bool operator<	(const xstring& s)	const;
+		bool operator<	(const char *s)		const;
+		bool operator<=	(const xstring& s)	const;
+		bool operator<=	(const char *s)		const;
+		bool operator>	(const xstring& s)	const;
+		bool operator>	(const char *s)		const;
+		bool operator>=	(const xstring& s)	const;
+		bool operator>=	(const char *s)		const;
+		char& operator[] (int i) const;
 		friend std::ostream& operator<< (std::ostream& stream, const xstring& s);
+		friend std::istream& operator>> (std::istream& stream, const xstring& s);
 
-		int size() const;
+		xstring& append(const xstring& s);
+		xstring& append(const xstring& s, int i);
+		xstring& append(const char *s);
+		xstring& append(const char *s, int i);
+
+		char *cstr() const;
+		char& last() const;
+		std::size_t size();
+		bool empty() const;
 		void clear();
 };
+
+template<typename T> xstring tostr(T val);
+template<typename T> const char *getformat();
+template<typename T>
+xstring tostr(T val)
+{
+	char buf[25];
+	snprintf(buf, sizeof(buf), getformat<T>(), val);
+	return xstring(buf);
+}
 
 #endif /* XSTRING_H */
