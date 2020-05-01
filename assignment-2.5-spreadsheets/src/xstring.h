52138f3c391a49cf7442f80a60d506f6857d0acb
Christos Margiolis <christos@margiolis.net>
2020-05-01 19:21:01 +0300

    pending possible bug fix on line 96 in xstring.cpp

--- assignment-2.5-spreadsheets/src/xstring.h
+++ assignment-2.5-spreadsheets/src/xstring.h
@@ -6,14 +6,17 @@
 #include <iostream>
 #include <string.h>
 
+namespace lab {
 class xstring
 {
 	private:
 		char *str;
-		int len;
+		std::size_t len;
 
 		char *conv(const char *s);
+		char *conv(char c);
 		void resize(const char *s);
+		void resize(std::size_t n);
 		bool strempty(const char *s) const;
 
 	public:
@@ -28,6 +31,7 @@ class xstring
 		xstring  operator+	(const char *s);
 		xstring& operator+=	(const xstring& s);
 		xstring& operator+=	(const char *s);
+		xstring& operator+=	(char c);
 		bool operator==	(const xstring& s)	const;
 		bool operator== (const char *s)		const;
 		bool operator!=	(const xstring& s)	const;
@@ -40,14 +44,15 @@ class xstring
 		bool operator>	(const char *s)		const;
 		bool operator>=	(const xstring& s)	const;
 		bool operator>=	(const char *s)		const;
-		char& operator[] (int i) const;
+		char& operator[] (std::size_t i) const;
 		friend std::ostream& operator<< (std::ostream& stream, const xstring& s);
 		friend std::istream& operator>> (std::istream& stream, const xstring& s);
 
 		xstring& append(const xstring& s);
-		xstring& append(const xstring& s, int i);
+		xstring& append(const xstring& s, std::size_t i);
 		xstring& append(const char *s);
-		xstring& append(const char *s, int i);
+		xstring& append(const char *s, std::size_t i);
+		xstring& append(char c);
 
 		char *cstr() const;
 		char& last() const;
@@ -56,14 +61,27 @@ class xstring
 		void clear();
 };
 
-template<typename T> xstring tostr(T val);
+std::istream& getline(std::istream& stream, xstring& s);
+std::istream& getline(std::istream& stream, xstring& s, char delim);
+
+template<typename T> xstring to_xstr(T val);
 template<typename T> const char *getformat();
 template<typename T>
-xstring tostr(T val)
+xstring to_xstr(T val)
 {
 	char buf[25];
 	snprintf(buf, sizeof(buf), getformat<T>(), val);
 	return xstring(buf);
+}
+
+// set a default T
+template<typename T>
+xstring to_xstr(const char *fs, T val)
+{
+	char buf[25];
+	snprintf(buf, sizeof(buf), fs, val);
+	return xstring(buf);
+}
 }
 
 #endif /* XSTRING_H */
