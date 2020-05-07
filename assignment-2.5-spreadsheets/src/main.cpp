a0c986a5c03ceef781a27ed89f2c05c9499e5271
Christos Margiolis <christos@margiolis.net>
2020-05-07 10:37:43 +0300

    changed template specializations to std::is_same

--- assignment-2.5-spreadsheets/src/main.cpp
+++ assignment-2.5-spreadsheets/src/main.cpp
@@ -1,20 +1,20 @@
-#include <fstream>
-#include <vector>
-#include "xstring.h"
+#include "app.h"
+
+std::ostream& operator<< (std::ostream& stream, const App& app);
 
 int main(int argc, char **argv)
 {
-	std::ifstream f("res/grades.csv");
-	while (f.good())
-	{
-		lab::xstring a, b, c;
-		lab::getline(f, a, ';');
-		lab::getline(f, b, ';');
-		lab::getline(f, c);
-		if (f.eof()) break;
-		std::cout << a << " " << b << " " << std::atof(c.cstr()) << std::endl;
-	}
-	f.close();
+	App app;
+	if (!app.import_data<Course>("res/courses.csv")) return -1;
+	if (!app.import_data<Grades>("res/grades.csv")) return -1;
+	if (!app.import_data<Student>("res/students.csv")) return -1;
+	if (!app.import_matchings("res/matchings.csv")) return -1;
+	//std::cout << app << std::endl;
 
 	return 0;
+}
+
+std::ostream& operator<< (std::ostream& stream, const App& app)
+{
+	return stream;	
 }
