5db9bc0d12ffae108f3f29af800bb9606410f84c
Christos Margiolis <christos@margiolis.net>
2020-06-04 03:58:46 +0300

    added some smart pointers

--- assignment-2.5-spreadsheets/src/main.cpp
+++ assignment-2.5-spreadsheets/src/main.cpp
@@ -1,15 +1,16 @@
+#include <memory>
 #include "datahandler.h"
 
 int
 main(int argc, char **argv)
 {
-    DataHandler dh;
-    if (!dh.import_data<Course>("res/courses.csv")) return -1;
-    if (!dh.import_data<Student>("res/students.csv")) return -1;
-    if (!dh.import_data<equivalencies>("res/equivalencies.csv")) return -1;
-    if (!dh.store_data()) return -1;
-    if (!dh.make_report()) return -1;
-    dh.summary();
+    std::unique_ptr<DataHandler> dh = std::make_unique<DataHandler>();
+    if (!dh->import_data<Course>("res/courses.csv")) return -1;
+    if (!dh->import_data<Student>("res/students.csv")) return -1;
+    if (!dh->import_data<equivalencies>("res/equivalencies.csv")) return -1;
+    if (!dh->store_data()) return -1;
+    if (!dh->make_report()) return -1;
+    dh->summary();
 
     return 0;
 }
