83fc17323da04663c84998900cb926802db36bb7
Christos Margiolis <christos@margiolis.net>
2025-09-23 20:32:24 +0300

    fixed some caps

--- assignment-2.3-operoverloading/src/subject.cpp
+++ assignment-2.3-operoverloading/src/subject.cpp
@@ -1,10 +1,10 @@
 #include "subject.h"
 
 
-Subject::Subject() {std::cout << "Constructor 1" << std::endl;}
+Subject::Subject() {}
 
 Subject::Subject(const std::string& code, const std::string& sname, unsigned int subjsemester)
-	:code(code), sname(sname), subjsemester(subjsemester) {std::cout << "Constructor 2" << std::endl;}
+	:code(code), sname(sname), subjsemester(subjsemester) {}
 
 Subject::Subject(const Subject& s)
-	:code(s.code), sname(s.sname), subjsemester(s.subjsemester) {std::cout << "Copy Constructor" << std::endl;}
+	:code(s.code), sname(s.sname), subjsemester(s.subjsemester) {}
