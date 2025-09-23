c2f6e4914ad4df7b42b6532eec4dfdba808119c5
Christos Margiolis <christos@margiolis.net>
2025-09-23 20:32:27 +0300

    renplaced - with _

---
+++ c_programming/5_arrays_ptrs_files/minecurses/src/audio.h
@@ -0,0 +1,13 @@
+#ifndef AUDIO_H
+#define AUDIO_H
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_mixer.h>
+
+#define AUDIO_PATH "res/detective_8bit.wav"
+
+void *play_audio(void *tid);
+void volume		(char);
+void pause_audio(void);
+
+#endif /* AUDIO_H */
